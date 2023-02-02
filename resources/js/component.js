6600a1d4af71ce7a6541c95879bedab64cd796d1
Aaron J <am@jonesiscoding.com>
2023-02-01 21:34:20 -0600

    Adds with method for component creation and attachment.

--- resources/js/component.js
+++ resources/js/component.js
@@ -8,4 +8,13 @@ class Component {
   get id() {
     return this.el.id;
   }
+
+  static with(el, options) {
+    el.fn = el.fn || {};
+    if (undefined === el.fn[this.name]) {
+      Object.defineProperty(el.fn, this.name, { value: new this(el, options) });
+    }
+
+    return el.fn[this.name];
+  }
 }
